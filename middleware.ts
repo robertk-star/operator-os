6e083af68ed9719c47802addfca8544d247bb0ce
robertk-star <robertk@ardykay.com>
2026-09-14 10:59:17 -0500

    Refresh auth cookies on API routes

--- middleware.ts
+++ middleware.ts
@@ -43,5 +43,5 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/app/:path*", "/setup", "/login"],
+  matcher: ["/app/:path*", "/api/:path*", "/setup", "/login"],
 };
